5e49076a0019e8dacca95db510965f99afbe1465
Samuel <samuelketema01@gmail.com>
2024-04-02 01:00:01 +0300

    Background color is added to all

--- lib/src/widgets/bottom_navigation_bar.dart
+++ lib/src/widgets/bottom_navigation_bar.dart
@@ -5,6 +5,7 @@ class CustomBottomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      backgroundColor: const Color.fromARGB(255, 146, 146, 2),
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
           icon: Icon(Icons.home),
@@ -15,7 +16,15 @@ class CustomBottomNavigationBar extends StatelessWidget {
           label: 'Category',
         ),
         BottomNavigationBarItem(
+          icon: Icon(Icons.add),
+          label: 'add',
+        ),
+        BottomNavigationBarItem(
           icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+       BottomNavigationBarItem(
+          icon: Icon(Icons.person ),
           label: 'Search',
         ),
       ],
